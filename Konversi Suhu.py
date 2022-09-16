5b6767b825d6cd1b36efcd4dbb951ed1d34a8920
achmadrafly <113223443+achmadrafly@users.noreply.github.com>
2022-09-16 15:12:59 +0700

    Add files via upload

---
+++ Konversi Suhu.py
@@ -0,0 +1,11 @@
+Celcius = float(input("Masukkan besar suhu celcius "))
+print()
+
+Reamur = ((4/5) * Celcius)
+print("Hasil konversi ke reamur adalah : ",Reamur)
+
+Fahrenheit = ((9/5) * Celcius)+32
+print("Hasil konversi ke fahrenheit adalah : ",Fahrenheit)
+
+Kelvin = Celcius + 273
+print("Hasil Konversi ke kelvin adalah : ",Kelvin)
